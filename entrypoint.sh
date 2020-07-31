db555be115a71f58f689cb78d821bcfccd12101b
manish-jaiswal15 <38095603+manish-jaiswal15@users.noreply.github.com>
2020-07-31 22:07:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ envsubst < ./newrelic/newrelic.yml.template > ./newrelic/newrelic.yml
 if [[ "$#" -eq 0 ]]
 then
     echo "Starting Connect..."
-    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/sink.properties 2>&1
+    exec /opt/kafka_2.11-0.11.0.2/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/sink.properties 2>&1
 else
     /bin/bash -c "$*"
 fi
